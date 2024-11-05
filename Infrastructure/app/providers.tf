bd1b359abab8068add1e57b6aa1219e2f4592764
Dhruv Darji <dhruvdarji145@gmail.com>
2024-11-05 09:48:47 +0530

    009

--- Infrastructure/app/providers.tf
+++ Infrastructure/app/providers.tf
@@ -1,8 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
-  # shared_credentials_files = ["~/.aws/credentials"] 
-  profile                  = "Dhruv@Dev"            
 
   default_tags {
     tags = {
